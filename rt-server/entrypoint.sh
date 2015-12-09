8adf2a57a8fb12266b46f4ba678b4c31460fa4b9
Lars Kellogg-Stedman <lars@redhat.com>
2015-12-09 17:46:16 -0500

    moved db initialization to another script

--- rt-server/entrypoint.sh
+++ rt-server/entrypoint.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 
+. /scripts/functions.sh
+
+MY_ADDRESS=$(ip addr show eth0 |
+	awk '$1 == "inet" {print $2}' |
+	cut -f1 -d/)
+
+: ${RT_DOMAIN:=localhost}
+: ${RT_WEBDOMAIN:=$MY_ADDRESS}
+
+export RT_DOMAIN RT_WEBDOMAIN
+
 /scripts/generate-msmtprc
 /scripts/generate-rt-config
 /scripts/db/wait
+/scripts/db/check || /scripts/initialize-db
 
 cat > /etc/apache2/conf.d/servername.conf <<EOF
-ServerName ${RT_DOMAIN:-localhost}
+ServerName ${RT_WEBDOMAIN}
 EOF
 
-if ! /scripts/db/check; then
-	rt-setup-database --action init --skip-create
-	/scripts/load-sitedata
-fi
-
+info 'setting password for rt root user'
 /scripts/rt-passwd root "$RT_ROOT_PASSWORD"
+
+info 'configuring apache log directory'
+mkdir -p /var/log/apache2
+chown -R apache:apache /var/log/apache2
 
 exec "$@"
 
